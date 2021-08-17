0c2a343d3f5a418ab94434e98676ff82fee3a288
Yulie Jung <yuliekorea@gmail.com>
2021-08-18 03:01:07 +0900

    add register api

--- flask-server/api/user.py
+++ flask-server/api/user.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from models import User
 from db_connect import db
+from sqlalchemy.exc import IntegrityError
 
 
 users_blueprint = Blueprint("users", __name__)
@@ -16,3 +17,20 @@ def testMigration():
     user = User("aaa", "asdfasdf", "Yulie")
     db.session.add(user)
     db.session.commit()
+
+
+# Register new user
+@users_blueprint.route("/users", methods=["POST"])
+def register():
+    try:
+        user_data = request.json
+
+        new_user = User(
+            user_data.get("email"), user_data.get("password"), user_data.get("name")
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"result": 0, "message": "success"})
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({"result": 1, "message": "existing user"})
