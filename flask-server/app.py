bbeb17fc427321d9b0f546f66c7bcfd4f8fa9e71
Yulie Jung <yuliekorea@gmail.com>
2021-08-17 23:21:03 +0900

    connect to local db

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 
+# Create a Flask instance
 app = Flask(__name__)
 
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "mysql+pymysql://root:root@localhost/elice_portfolio_db"
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "Hello World!"
