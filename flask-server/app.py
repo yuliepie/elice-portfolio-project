c5bea75c4d01f44a9e1120ad92746ca302342093
Yulie Jung <yuliekorea@gmail.com>
2021-08-17 23:40:48 +0900

    config db in app

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from db_connect import db  # import SQLAlchemy object
+
+db_migration = Migrate()
 
 # Create a Flask instance
 app = Flask(__name__)
@@ -6,6 +11,14 @@ app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = "mysql+pymysql://root:root@localhost/elice_portfolio_db"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = "BLKJSSLDIJLJFLKSJDFLIJ"
+
+db.init_app(app)
+db_migration.init_app(app, db)
+
+bcrypt = Bcrypt(app)
 
 
 @app.route("/")
