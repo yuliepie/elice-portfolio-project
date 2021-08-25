01df61b0ae05b665b8814574b851a352292ebbe3
yuliepie <yuliekorea@gmail.com>
2021-08-25 23:51:10 +0900

    update create_db

--- flask-server/create_db.py
+++ flask-server/create_db.py
@@ -1,14 +1,17 @@
 import mysql.connector
+import os
 
-DB_NAME = "elice_portfolio_db"
+DB_NAME = os.getenv("DATABASE_NAME", default="elice_portfolio_db")
+DB_HOST = os.getenv("DATABASE_NAME", default="localhost")
+DB_USER = os.getenv("DATABASE_NAME", default="root")
+DB_PASSWORD = os.getenv("DATABASE_NAME", default="root")
 
-mydb = mysql.connector.connect(host="localhost", user="root", passwd="root")
+mydb = mysql.connector.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWORD)
 print("connected!")
 
 cursor = mydb.cursor()
-
-# cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
-# print("Database {} created successfully.".format(DB_NAME))
+cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+print("Database {} created successfully.".format(DB_NAME))
 
 mydb.database = DB_NAME
 
