d5efbe6afb67544c4385135e8427dcac2151fe0a
yuliepie <yuliekorea@gmail.com>
2021-08-26 00:50:00 +0900

    separate seeding script

--- flask-server/seed_db.py
+++ flask-server/seed_db.py
@@ -9,9 +9,13 @@ DB_PASSWORD = os.getenv("DATABASE_NAME", default="root")
 mydb = mysql.connector.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWORD)
 print("connected!")
 
+mydb.database = DB_NAME
 cursor = mydb.cursor()
-cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
-print("Database {} created successfully.".format(DB_NAME))
+
+# Seed eduation status codes
+statuses = ["재학중", "학사졸업", "석사졸업", "박사졸업"]
+for status in statuses:
+    cursor.execute(f"INSERT INTO education_status(status_name) VALUES('{status}')")
 
 mydb.commit()
 cursor.close()
